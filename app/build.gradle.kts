4450e951dca6efa52815ae485445bda5b87f86f0
burgerbytes <oteroantoniogomez@gmail.com>
2024-03-02 12:14:47 -0500

    add prompt box

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
 }
--- app/src/main/java/com/example/memento/userinterface/Home/HomeView.kt
+++ app/src/main/java/com/example/memento/userinterface/Home/HomeView.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -38,6 +41,16 @@ fun HomeView(
                 fontSize = MaterialTheme.typography.titleMedium.fontSize,
                 fontWeight = FontWeight.Bold,
                 color = Color.White
+            )
+            OutlinedTextField(
+                value = "Prompt diario",
+                onValueChange = {},
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp)
             )
             Row(
                 horizontalArrangement = Arrangement.Center,
