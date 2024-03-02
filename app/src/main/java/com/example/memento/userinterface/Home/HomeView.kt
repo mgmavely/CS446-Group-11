f598785a730ac88e5b7cd8124f0c99d1ffa992d2
burgerbytes <oteroantoniogomez@gmail.com>
2024-03-02 13:37:38 -0500

    add images for streak and time remaining

--- app/src/main/java/com/example/memento/userinterface/Home/HomeView.kt
+++ app/src/main/java/com/example/memento/userinterface/Home/HomeView.kt
@@ -1,31 +1,28 @@
 package org.example.userinterface.Home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.R
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 
 @Preview
 @Composable
 fun HomeView(
     onHomeClicked: () -> Unit = {}
 ) {
+    var daysPressed by remember { mutableStateOf(5) } // Simulación de días seguidos
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -53,6 +50,34 @@ fun HomeView(
                     .padding(vertical = 20.dp)
             )
             Row(
+                horizontalArrangement = Arrangement.Center, // Center the columns
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 40.dp, horizontal = 50.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        painter = painterResource(id = com.example.memento.R.drawable.ic_lightning),
+                        contentDescription = "streak",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Text("$daysPressed")
+                }
+                Spacer(modifier = Modifier.width(80.dp)) // Add spacing between columns
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        painter = painterResource(id = com.example.memento.R.drawable.ic_clock),
+                        contentDescription = "time remaining",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Text("hours left")
+                }
+            }
+            Row(
                 horizontalArrangement = Arrangement.Center,
                 modifier = Modifier
                     .fillMaxSize()
@@ -65,3 +90,4 @@ fun HomeView(
         }
     }
 }
+
