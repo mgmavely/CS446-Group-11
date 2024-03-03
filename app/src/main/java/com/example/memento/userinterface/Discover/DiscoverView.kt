e411ce4ccfe81d84eea8edbfce62245323f1a86d
Jackson Howe <jacksonhowe3@gmail.com>
2024-03-03 12:48:58 -0500

    skeleton of discover page

--- app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
+++ app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,25 +20,50 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+
 @Preview
 @Composable
 fun DiscoverView() {
+    val currentPrompt = null;
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Yellow)
+            .background(MaterialTheme.colorScheme.background)
             .padding(top = 20.dp, start = 20.dp, end = 20.dp)
     ) {
+        // Prompt at the top of the screen that doesn't change
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .height(20.dp)
+                .background(Color.Red)
         ) {
             Text(
-                text = "Discover",
-                fontSize = MaterialTheme.typography.titleMedium.fontSize,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
+                text = "Fixed Box",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
             )
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 20.dp)
+                .background(Color.Green)
+        ) {
+            items(100) {
+                // Your lazy column content here
+                Text(
+                    text = "Item $it",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    fontWeight = FontWeight.Normal
+                )
+            }
         }
     }
 }
