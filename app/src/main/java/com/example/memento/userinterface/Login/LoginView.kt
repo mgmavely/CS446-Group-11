d83d8ff52e55fa4ca9c6ef53bf80aa9d63bb8859
Jeffery Le <jefferyle02@gmail.com>
2024-03-03 02:26:40 -0500

    updated LoginView.kt by tweaking visuals and adding sign up button

--- app/src/main/java/com/example/memento/userinterface/Login/LoginView.kt
+++ app/src/main/java/com/example/memento/userinterface/Login/LoginView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
@@ -16,6 +17,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -26,11 +28,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,28 +47,45 @@ fun LoginView(
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Login Screen") })
+            CenterAlignedTopAppBar(
+                    title = { Text("MEMENTO", textAlign = TextAlign.Center, fontSize = 65.sp, lineHeight = 33.sp)
+
+                            },
+                    modifier = Modifier.padding(top=180.dp, bottom=100.dp)
+            )
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.fillMaxWidth()
-            .padding(innerPadding),
+            modifier = Modifier.fillMaxWidth().padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly) {
+            ) {
+
             TextField(
                 username,
                 label = {Text("Username: ")},
-                onValueChange = { username = it })
+                onValueChange = { username = it },
+
+                    )
+
             TextField(
                 password,
                 label = {Text("Password: ")},
                 onValueChange = { password = it },
+
                 visualTransformation =  PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password))
 
-            Row (horizontalArrangement = Arrangement.SpaceEvenly) {
-                Button(onClick = onLoginButtonClicked) {
-                    Text("Log In")
+            Row () {
+                Button(
+                        onClick = onLoginButtonClicked,
+                        modifier = Modifier.padding(12.dp),
+                        ) {Text(" Log In ", fontSize = 22.sp)}
+
+            }
+            Text("or", fontSize = 15.sp)
+            Row () {
+                Button(onClick = onLoginButtonClicked, modifier = Modifier.padding(top=12.dp),) {
+                    Text("Sign Up", fontSize = 22.sp)
                 }
             }
         }
