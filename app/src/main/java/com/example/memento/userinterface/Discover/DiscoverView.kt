7758d3f4deb84ec3e5f4dcac506dc3302e6979b3
Jackson Howe <jacksonhowe3@gmail.com>
2024-03-03 13:46:57 -0500

    get autoresizing text

--- app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
+++ app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
@@ -10,34 +10,54 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.isUnspecified
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun Prompt() {
-    var promptText = "Post a picture of your favourite animal"
+    var promptText = "" +
+            "Post a picture of your favourite animal" +
+            "Post a picture of your favourite animal" +
+            "Post a picture of your favourite animal" +
+            "Post a picture of your favourite animal" +
+            "Post a picture of your favourite animal" +
+            "Post a picture of your favourite animal"
 
-    Column(
+            Box(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight(0.2f)
-            .background(Color.Red)
+            .background(MaterialTheme.colorScheme.primaryContainer)
     ) {
-        Text(
-            text = "Fixed Box",
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp),
-            color = Color.Black,
-            fontWeight = FontWeight.Bold
-        )
+                AutoResizingText(
+                    modifier = Modifier.
+                        padding(10.dp),
+                    text = promptText,
+                    color = MaterialTheme.colorScheme.primary,
+                    targetTextSize = 30.sp
+                )
     }
 }
 
@@ -73,3 +93,35 @@ fun DiscoverView() {
     }
 }
 
+@Composable
+fun AutoResizingText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color,
+    targetTextSize: TextUnit,
+    maxLines: Int = Int.MAX_VALUE
+) {
+
+    val textSize = remember {
+        mutableStateOf(targetTextSize)
+    }
+
+    Text(
+        modifier = modifier,
+        text = text,
+        color = color,
+        textAlign = TextAlign.Start,
+        fontSize = textSize.value,
+        maxLines = maxLines,
+        overflow = TextOverflow.Ellipsis,
+        onTextLayout = {textLayoutResult ->
+            val lineIndex = textLayoutResult.lineCount - 1
+
+            if (textLayoutResult.isLineEllipsized(lineIndex)) {
+                textSize.value = textSize.value * 0.9f
+            }
+        }
+    )
+
+
+}
