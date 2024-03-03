59ace539173bc27524cb0e5ac3dbcda4d0621bd7
Jackson Howe <jacksonhowe3@gmail.com>
2024-03-03 14:43:04 -0500

    add image to post

--- app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
+++ app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
@@ -1,39 +1,33 @@
 package org.example.userinterface.Equipment
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.isUnspecified
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
-
+import com.example.memento.R
 
 @Composable
 fun Prompt() {
@@ -54,6 +48,15 @@ fun Prompt() {
                     targetTextSize = 30.sp
                 )
     }
+}
+
+@Composable
+fun Post(it: Int) {
+    val image = painterResource(R.drawable.baseline_public_24)
+    Image(
+        painter = image,
+        contentDescription = "post"
+    )
 }
 
 @Preview
@@ -80,14 +83,7 @@ fun DiscoverView() {
                 .background(Color.Green)
         ) {
             items(100) {
-                // Your lazy column content here
-                Text(
-                    text = "Item $it",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    fontWeight = FontWeight.Normal
-                )
+                Post(it)
             }
         }
     }
@@ -124,6 +120,4 @@ fun AutoResizingText(
         fontWeight = FontWeight.Normal,
         lineHeight = 30.sp
     )
-
-
 }
