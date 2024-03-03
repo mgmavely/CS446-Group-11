2a727cbc11fb03e3fa1284b86e3b63cbcfc2fcbb
Jackson Howe <jacksonhowe3@gmail.com>
2024-03-03 17:39:34 -0500

    add hardcoded pictures

--- app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
+++ app/src/main/java/com/example/memento/userinterface/Discover/DiscoverView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -29,9 +30,32 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.example.memento.R
 
+class ImageCollection() {
+    val painters: MutableList<Int> = mutableListOf()
+    init {
+        painters.add(R.drawable.dog)
+        painters.add(R.drawable.campfire)
+        painters.add(R.drawable.flowers)
+        painters.add(R.drawable.forest)
+        painters.add(R.drawable.ocean)
+    }
+
+    fun getAll(): MutableList<Int> {
+        return painters
+    }
+
+    fun getPainter(index: Int): Int {
+        return if (index in 0 until painters.size) {
+            painters[index]
+        } else {
+            -1
+        }
+    }
+}
+
 @Composable
 fun Prompt() {
-    var promptText = "Put prompt here to check text width"
+    var promptText = "Take a picture of something beautiful..."
 
     Box(
         modifier = Modifier
@@ -53,22 +77,38 @@ fun Prompt() {
 @Composable
 fun Post(
     modifier: Modifier = Modifier,
-    it: Int
+    imagePath: Int
 ) {
-    val image = painterResource(id = R.drawable.dog)
-    Image(
-        painter = image,
-        contentDescription = "post"
-    )
+    val image = painterResource(id = imagePath)
+
+    Box(
+        modifier = modifier
+            .size(380.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.background)
+            .padding(10.dp)
+    ) {
+        Image(
+            painter = image,
+            contentDescription = "post",
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(8.dp))
+        )
+    }
+
 }
 
 @Preview
 @Composable
 fun DiscoverView() {
+    // All images to show as posts
+    val images = ImageCollection()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+            .background(MaterialTheme.colorScheme.tertiary)
             .padding(top = 10.dp, start = 10.dp, end = 10.dp),
         verticalArrangement = Arrangement.spacedBy(1.dp)
     ) {
@@ -83,12 +123,14 @@ fun DiscoverView() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = 10.dp)
-                .background(Color.Green)
+                .background(MaterialTheme.colorScheme.tertiary),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            items(100) {
+            items(5) {
                 Post(
                     Modifier,
-                    it
+                    images.getPainter(it)
                 )
             }
         }
