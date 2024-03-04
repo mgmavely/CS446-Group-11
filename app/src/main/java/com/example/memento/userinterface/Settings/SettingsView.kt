b83dcbf987dbb94dd4d8019edbcee34ca5c66d53
Jeffery Le <jefferyle02@gmail.com>
2024-03-03 22:19:20 -0500

    applied theme update to settings and login page

--- app/src/main/java/com/example/memento/userinterface/Settings/SettingsView.kt
+++ app/src/main/java/com/example/memento/userinterface/Settings/SettingsView.kt
@@ -1,8 +1,10 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 package org.example.userinterface.Settings
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
@@ -37,7 +39,9 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import com.example.memento.theme.MementoTheme
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.SwitchDefaults
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
@@ -46,99 +50,161 @@ fun SettingsView(
         onMentalHealthOnlineClicked: () -> Unit = {},
         onMentalHealthPhoneClicked: () -> Unit = {}
 ) {
-    Scaffold(
+    MementoTheme {
+
+        Scaffold(
             topBar = {
                 CenterAlignedTopAppBar(
-                        title = { Text("Settings", textAlign = TextAlign.Center, fontSize = 40.sp)
+                    title = {
+                        Text("SETTINGS", textAlign = TextAlign.Center, fontSize = 40.sp
+                        ,color = MaterialTheme.colorScheme.onBackground,)
 
-                        },
-                        modifier = Modifier.padding(25.dp)
+                    },
+                    modifier = Modifier.padding(25.dp)
                 )
             }
-    ) { innerPadding ->
-        Column(
-                modifier = Modifier.fillMaxWidth().padding(innerPadding)
-        ) {
-            Divider(thickness = 3.dp)
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(innerPadding)
+            ) {
+                Divider(thickness = 3.dp)
 
-            var checked by remember { mutableStateOf(true) }
+                var checked by remember { mutableStateOf(true) }
 
-            Column () {
-                Text("Enable Dark Mode", fontSize = 18.sp,
-                        modifier = Modifier.padding(horizontal=10.dp))
-                Switch(
+                Column() {
+                    Text(
+                        "Enable Dark Mode", fontSize = 18.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
+                    Switch(
                         checked = false,
-                        modifier = Modifier.padding(horizontal=25.dp),
+                        colors = SwitchDefaults.colors(checkedTrackColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier.padding(horizontal = 25.dp),
                         onCheckedChange = {
                             checked = it
                         }
-                )
-                Divider(thickness = 1.dp)
-            }
+                    )
+                    Divider(thickness = 1.dp)
+                }
 
 
-            Column () {
-                Text("Notifications", fontSize = 18.sp,
-                    modifier = Modifier.padding(horizontal=10.dp))
+                Column(modifier = Modifier) {
+                    Text(
+                        "Notifications", fontSize = 18.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
 
-                Text("Enable Notifications", fontSize = 15.sp,
-                    modifier = Modifier.padding(horizontal=10.dp))
-                Switch(
-                    checked = checked,
-                    modifier = Modifier.padding(horizontal=25.dp),
-                    onCheckedChange = {
-                        checked = it
+                    Text(
+                        "Enable Notifications", fontSize = 15.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
+                    Switch(
+                        checked = checked,
+                        colors = SwitchDefaults.colors(checkedTrackColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier.padding(horizontal = 25.dp),
+                        onCheckedChange = {
+                            checked = it
+                        }
+                    )
+
+                    Text(
+                        "Temporarily Mute Notifications", fontSize = 15.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
+                    Switch(
+                        checked = false,
+                        colors = SwitchDefaults.colors(checkedTrackColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier.padding(horizontal = 25.dp),
+                        onCheckedChange = {
+                            checked = it
+                        }
+                    )
+
+                    Divider(thickness = 1.dp)
+                }
+
+                Column() {
+                    Text(
+                        "Mental Health Resources", fontSize = 18.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
+                    Button(
+                        onClick = onMentalHealthOnlineClicked,
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier.padding(top = 12.dp).padding(horizontal = 25.dp),
+                    ) {
+                        Text("Online Mental Health Resources", fontSize = 14.sp)
                     }
-                )
-
-                Text("Temporarily Mute Notifications", fontSize = 15.sp,
-                    modifier = Modifier.padding(horizontal=10.dp))
-                Switch(
-                    checked = false,
-                    modifier = Modifier.padding(horizontal=25.dp),
-                    onCheckedChange = {
-                        checked = it
+                    Button(
+                        onClick = onMentalHealthPhoneClicked,
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .padding(horizontal = 25.dp),
+                    ) {
+                        Text("Mental Health Related Phone Lines", fontSize = 14.sp)
                     }
-                )
+                    Divider(thickness = 1.dp)
+                }
 
-                Divider(thickness = 1.dp)
+                Column() {
+                    Text(
+                        "Legal", fontSize = 18.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
+                    Button(
+                        onClick = onMentalHealthOnlineClicked,
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier
+                            .padding(top = 12.dp)
+                            .padding(horizontal = 25.dp),
+                    ) {
+                        Text("Privacy Policy", fontSize = 14.sp)
+                    }
+                    Button(
+                        onClick = onMentalHealthPhoneClicked,
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .padding(horizontal = 25.dp),
+                    ) {
+                        Text("Terms Of Service", fontSize = 14.sp)
+                    }
+                    Divider(thickness = 1.dp)
+                }
+
+                Column() {
+                    Button(
+                        onClick = onMentalHealthOnlineClicked,
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .align(alignment = Alignment.CenterHorizontally),
+                    ) {
+                        Text("Log Out", fontSize = 14.sp)
+                    }
+                    Divider(thickness = 1.dp)
+                    Button(
+                        onClick = onMentalHealthPhoneClicked,
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .align(alignment = Alignment.CenterHorizontally),
+                    ) {
+                        Text("Delete Account", fontSize = 14.sp)
+                    }
+                    Divider(thickness = 1.dp)
+                }
+
             }
-
-            Column () {
-                Text("Mental Health Resources", fontSize = 18.sp,
-                    modifier = Modifier.padding(horizontal=10.dp))
-                Button(onClick = onMentalHealthOnlineClicked, modifier = Modifier.padding(top=12.dp).padding(horizontal=25.dp),) {
-                    Text("Online Mental Health Resources", fontSize = 14.sp)
-                }
-                Button(onClick = onMentalHealthPhoneClicked, modifier = Modifier.padding(top=5.dp).padding(horizontal=25.dp),) {
-                    Text("Mental Health Related Phone Lines", fontSize = 14.sp)
-                }
-                Divider(thickness = 1.dp)
-            }
-
-            Column () {
-                Text("Legal", fontSize = 18.sp,
-                    modifier = Modifier.padding(horizontal=10.dp))
-                Button(onClick = onMentalHealthOnlineClicked, modifier = Modifier.padding(top=12.dp).padding(horizontal=25.dp),) {
-                    Text("Privacy Policy", fontSize = 14.sp)
-                }
-                Button(onClick = onMentalHealthPhoneClicked, modifier = Modifier.padding(top=5.dp).padding(horizontal=25.dp),) {
-                    Text("Terms Of Service", fontSize = 14.sp)
-                }
-                Divider(thickness = 1.dp)
-            }
-
-            Column () {
-                Button(onClick = onMentalHealthOnlineClicked, modifier = Modifier.padding(vertical=10.dp).align(alignment=Alignment.CenterHorizontally),) {
-                    Text("Log Out", fontSize = 14.sp)
-                }
-                Divider(thickness = 1.dp)
-                Button(onClick = onMentalHealthPhoneClicked, modifier = Modifier.padding(vertical=10.dp).align(alignment=Alignment.CenterHorizontally),) {
-                    Text("Delete Account", fontSize = 14.sp)
-                }
-                Divider(thickness = 1.dp)
-            }
-
         }
     }
 }
