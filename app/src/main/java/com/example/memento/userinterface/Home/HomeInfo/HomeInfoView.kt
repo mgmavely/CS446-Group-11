b5e45c617528f5e4b1b180776a6a9cb21cb7cc10
mgmavely <mgmavely@gmail.com>
2024-03-06 21:14:22 -0500

    Fixed M -> M3 import error

--- app/src/main/java/com/example/memento/userinterface/Home/HomeInfo/HomeInfoView.kt
+++ app/src/main/java/com/example/memento/userinterface/Home/HomeInfo/HomeInfoView.kt
@@ -8,9 +8,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
