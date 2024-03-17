29a23ce6b3c2cd14ebb8912226da70d4a5cb7891
Carlos Sánchez <carlossanchezarroyo@gmail.com>
2024-03-17 19:15:28 -0400

    Files created

---
+++ app/src/main/java/com/example/memento/ChildActivity.kt
@@ -0,0 +1,24 @@
+package com.example.memento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+class ChildActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent{
+
+        }
+    }
+}
