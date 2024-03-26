46658219d050ce1192462a8fb2b29a3849603ec3
mgmavely <mgmavely@gmail.com>
2024-03-26 10:35:31 -0400

    Fixed navbar

--- app/src/main/java/com/example/memento/MainScreen.kt
+++ app/src/main/java/com/example/memento/MainScreen.kt
@@ -1,35 +1,31 @@
 package org.example
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -38,11 +34,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
 import com.example.memento.R
+
 import org.example.userinterface.MenuBarGraph
 import org.example.userinterface.MenuBarOptions
 
@@ -61,7 +60,7 @@ fun MementoApp(
 
     Scaffold(
         bottomBar = {
-            if (showNav) WaitlessMenuBar(navController = navController)
+            if (showNav) MementoMenuBar(navController = navController)
         },
         containerColor = MaterialTheme.colorScheme.primary
     )
@@ -74,7 +73,7 @@ fun MementoApp(
 
 
 @Composable
-fun WaitlessMenuBar(navController: NavHostController) {
+fun MementoMenuBar(navController: NavHostController) {
     val screens = listOf(
         MenuBarOptions.Settings,
         MenuBarOptions.Home,
@@ -82,35 +81,60 @@ fun WaitlessMenuBar(navController: NavHostController) {
     )
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
-
-    Box(
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = MaterialTheme.colorScheme.secondary,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(16.dp)
     ) {
-        Surface(
-            shape = RoundedCornerShape(16.dp),
-            color = MaterialTheme.colorScheme.primary
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NavigationBar(
-                containerColor = MaterialTheme.colorScheme.secondary,
-                contentColor = MaterialTheme.colorScheme.tertiary,
-                tonalElevation = 0.dp
+            Box(
+                modifier = Modifier,
+                contentAlignment = Alignment.Center
             ) {
-                screens.forEach { screen ->
-                    AddItem(
-                        screen = screen,
-                        currentDestination = currentDestination,
-                        navController = navController
-                    )
+                Row(
+                    modifier = Modifier
+                        .padding(0.dp)
+                ) {
+                    screens.forEach { screen ->
+                        AddItem(
+                            screen = screen,
+                            currentDestination = currentDestination,
+                            navController = navController
+                        )
+                    }
                 }
             }
+//        Surface(
+//            shape = RoundedCornerShape(16.dp),
+//            color = MaterialTheme.colorScheme.primary
+//        ) {
+//            NavigationBar(
+//                containerColor = MaterialTheme.colorScheme.secondary,
+//                contentColor = MaterialTheme.colorScheme.tertiary,
+//                tonalElevation = 0.dp
+//            ) {
+//                screens.forEach { screen ->
+//                    AddItem(
+//                        screen = screen,
+//                        currentDestination = currentDestination,
+//                        navController = navController,
+//                    )
+//                }
+//            }
+//        }
         }
     }
 }
 
 @Composable
-fun RowScope.AddItem(
+fun AddItem(
     screen: MenuBarOptions,
     currentDestination: NavDestination?,
     navController: NavHostController
@@ -119,35 +143,68 @@ fun RowScope.AddItem(
         val test: String = it.route?: ""
         test.split("/")[0] == screen.route
     } == true
-    NavigationBarItem(
-        label = {
+
+    Box(
+        modifier = Modifier
+            .clickable(onClick = {
+                navController.navigate(screen.route) {
+                    popUpTo(navController.graph.findStartDestination().id)
+                    launchSingleTop = true
+                }
+            })
+            .padding(30.dp, 0.dp),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = if (selected) screen.icon_filled else screen.icon_outlined,
+                contentDescription = "Navigation Icon",
+                modifier = Modifier.size(32.dp),
+                tint = if (selected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
+            )
             Text(
                 text = screen.title,
                 style = MaterialTheme.typography.bodyMedium,
                 fontWeight = FontWeight.Bold,
                 color = if (selected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
             )
-        },
-        icon = {
-            Surface(
-                shape = RoundedCornerShape(16.dp),
-                color = Color.Transparent
-            ) {
-                Icon(
-                    imageVector = if (selected) screen.icon_filled else screen.icon_outlined,
-                    contentDescription = "Navigation Icon",
-                    modifier = Modifier.size(32.dp),
-                    tint = if (selected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
-                )
-            }
-               },
-        modifier = Modifier.fillMaxSize(),
-        selected = selected,
-        onClick = {
-            navController.navigate(screen.route) {
-                popUpTo(navController.graph.findStartDestination().id)
-                launchSingleTop = true
-            }
         }
-    )
+    }
+
+//
+//    NavigationBarItem(
+//        label = {
+//            Text(
+//                text = screen.title,
+//                style = MaterialTheme.typography.bodyMedium,
+//                fontWeight = FontWeight.Bold,
+//                color = if (selected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
+//            )
+//        },
+//        icon = {
+//            Surface(
+//                shape =  RoundedCornerShape(16.dp),
+//                color = Color.Transparent
+//            ) {
+//                Icon(
+//                    imageVector = if (selected) screen.icon_filled else screen.icon_outlined,
+//                    contentDescription = "Navigation Icon",
+//                    modifier = Modifier.size(32.dp),
+//                    tint = if (selected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
+//                )
+//            }
+//               },
+//        modifier = Modifier.fillMaxSize()
+//            .then(indicatorModifier),
+//        selected = selected,
+//        onClick = {
+//            navController.navigate(screen.route) {
+//                popUpTo(navController.graph.findStartDestination().id)
+//                launchSingleTop = true
+//            }
+//        }
+//    )
 }
