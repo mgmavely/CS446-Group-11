778208337a181c565c7da3222059c79ced3e74b9
Jeffery Le <jefferyle02@gmail.com>
2024-03-27 19:13:37 -0400

    implemented mvvm

--- app/src/main/java/com/example/memento/mvvm/viewmodel/HistoryViewModel.kt
+++ app/src/main/java/com/example/memento/mvvm/viewmodel/HistoryViewModel.kt
@@ -12,20 +12,51 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import org.example.userinterface.History.PostItem
 
+data class PostItem(
+    val promptQuestion: String?,
+    val promptAnswer: String?,
+    val date: String?,
+    val imageURL: String?
+)
 class HistoryViewModel : ViewModel() {
-    private val firebaseAuth = FirebaseAuth.getInstance()
     private val db = FirebaseFirestore.getInstance()
     private val storage = FirebaseStorage.getInstance()
 
-    val imageAvailable: MutableState<Boolean> = mutableStateOf(false)
+    private val _posts = MutableStateFlow<List<PostItem>>(emptyList())
+    val posts: StateFlow<List<PostItem>> = _posts
+
+    init {
+        loadPosts()
+    }
+
+    fun loadPosts() {
+        viewModelScope.launch {
+            val db = Firebase.firestore
+
+            // call to get posts
+            db.collection("posts").get().addOnSuccessListener { querySnapshot ->
+                val postsList = querySnapshot.map { document ->
+                    PostItem("Prompt q here",
+                        document.getString("caption"),
+                        document.getString("date"),
+                        document.getString("imageurl")
+                    )
+                }
+                _posts.value = postsList
+            }
 
 
-    fun setImageAvailable(newValue: Boolean) {
-        imageAvailable.value = newValue
+        }
+
     }
     fun deleteDocumentAndImage(deleteKey: String) {
         viewModelScope.launch {
--- app/src/main/java/com/example/memento/userinterface/History/History.kt
+++ app/src/main/java/com/example/memento/userinterface/History/History.kt
@@ -1,7 +1,5 @@
 package org.example.userinterface.History
 import android.annotation.SuppressLint
-import android.icu.text.SimpleDateFormat
-import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -9,27 +7,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -38,25 +30,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
-import androidx.core.content.FileProvider
 import coil.compose.rememberImagePainter
-import com.example.memento.BuildConfig
-import com.example.memento.R
-import com.example.memento.mvvm.viewmodel.DiscoverViewModel
+import com.example.memento.mvvm.viewmodel.HistoryViewModel
 import com.example.memento.theme.MementoTheme
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import org.example.userinterface.Equipment.ImageCollection
-import org.example.userinterface.Home.createImageFile
-import java.util.Date
-import java.util.Objects
+
 
 data class PostItem(
     val promptQuestion: String?,
@@ -70,55 +50,46 @@ fun PostDisplay(
     modifier: Modifier = Modifier,
     post: PostItem,
 ) {
-    /**
-     * Represents a single post in the scrolling column
-     */
+    val promptQuestion = post.promptQuestion
+    val promptAnswer = post.promptAnswer
+    val promptDate = post.date
+    val imageURL = post.imageURL
 
-
-        // val image = painterResource(id = post.image)
-        val promptQuestion = post.promptQuestion
-        val promptAnswer = post.promptAnswer
-        val promptDate = post.date
-        val imageURL = post.imageURL
-
-        Box(
-            modifier = modifier
-            .clip(RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.secondary)
-            .padding(10.dp)
+    Box(
+        modifier = modifier.clip(RoundedCornerShape(8.dp)).background(MaterialTheme.colorScheme.secondary).padding(10.dp)
 
         ) {
 
         Column(){
             Image(
                 painter = rememberImagePainter(imageURL),
-                contentDescription = "Today's Memento",
                 modifier = Modifier
-                    .fillMaxHeight().fillMaxSize()
+                    .fillMaxHeight()
+                    .fillMaxSize()
                     .clip(RoundedCornerShape(8.dp))
                     .height(250.dp)
-                    .padding(10.dp)
+                    .padding(10.dp),
+                contentDescription = "image"
             )
             if (promptDate != null && promptAnswer != null ) {
 
+                AutoResizingText(
+                    text = promptDate, targetTextSize = 12.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
 
-            AutoResizingText(
-                text = promptDate, targetTextSize = 12.sp,
-                color = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.padding(horizontal = 10.dp)
-            )
+                AutoResizingText(
+                    text = "Q:" + promptQuestion, targetTextSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
 
-            AutoResizingText(
-                text = "Q:" + promptQuestion, targetTextSize = 20.sp,
-                color = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.padding(horizontal = 10.dp)
-            )
-
-            AutoResizingText(
-                text = "A: " + promptAnswer, targetTextSize = 20.sp,
-                color = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.padding(horizontal = 10.dp)
-            )
+                AutoResizingText(
+                    text = "A: " + promptAnswer, targetTextSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
             }
 
 
@@ -133,43 +104,18 @@ fun PostDisplay(
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun HistoryView() {
+fun HistoryView(viewModel: HistoryViewModel = HistoryViewModel()) {
 
-    val viewModel = DiscoverViewModel()
+    val posts by viewModel.posts.collectAsState()
 
-    // Firebase
-    val db = Firebase.firestore
-
-    var isDataReady by remember { mutableStateOf(false) }
-    var postsLocal by remember { mutableStateOf<List<PostItem>>(emptyList()) }
-    // call to get posts
-    val postsFirebase = db.collection("posts")
-    postsFirebase.get().addOnSuccessListener { querySnapshot ->
-        for (document in querySnapshot) {
-            val caption = document.getString("caption")
-            val date = document.getString("date")
-            val imageURL = document.getString("imageurl")
-
-            postsLocal = postsLocal.toMutableList().apply { add((PostItem("Prompt q here", caption, date, imageURL))) }
-
-        }
-        isDataReady = true
-    }
-
-
-    if (isDataReady) {
-        Log.e("text", "$postsLocal.size")
-
-        MementoTheme {
-            Scaffold(
-                topBar = {
-                    CenterAlignedTopAppBar(
-                        title = {
-                            Text(
-                                "YOUR POST HISTORY" , textAlign = TextAlign.Center, fontSize = 30.sp,
+    MementoTheme {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text("YOUR POST HISTORY" , textAlign = TextAlign.Center, fontSize = 30.sp,
                                 color = MaterialTheme.colorScheme.onBackground,
                             )
-
                         },
                     )
                 }) {
@@ -181,21 +127,13 @@ fun HistoryView() {
                     verticalArrangement = Arrangement.spacedBy(30.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-
-                    items(4) {
-
-                        // add all items
-                        PostDisplay(
-                            Modifier,
-                            postsLocal[it]
-                        )
-
+                    items(posts.size) { index ->
+                        PostDisplay(Modifier, posts[index])
                     }
                 }
             }
         }
     }
-}
 
 
 @Composable
